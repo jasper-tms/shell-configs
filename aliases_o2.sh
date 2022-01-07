8882988bdba20b3660db9befca085eee6cd1147d
Jasper Phelps <jtmaniatesselvin@g.harvard.edu>
2022-01-06 20:18:08 -0500

    'karmareportlogs' checks time and mem usage for all logs in current dir

--- aliases_o2.sh
+++ aliases_o2.sh
@@ -13,6 +13,7 @@ alias karma="sshare -U"
 alias karmareport="sacct --format=jobid%9,jobName%30,state,MaxRSS,ReqMem%6,Elapsed%13,Timelimit,NCPUS%4,NodeList%20 --units=G -u jtm23"
 alias karmareportwide="sacct --format=jobid%9,jobName%50,state,MaxRSS,ReqMem%6,Elapsed%13,Timelimit,NCPUS%4,NodeList%20 --units=G -u jtm23"
 alias karmareportu="sacct --format=jobid%9,jobName%30,state,MaxRSS,ReqMem%6,Elapsed%13,Timelimit,NCPUS%4,NodeList%20 --units=G -u"
+alias karmareportlogs='sacct --format=jobid%9,jobName%30,state,MaxRSS,ReqMem%6,Elapsed%13,Timelimit,NCPUS%4,NodeList%20 --units=G -j $(ls *job.o* | sed "s/.*job\.o//" | tr "\n" ",")'
 
 
 #Watch loops
