2527616866968931b4672fd47a15ef63f0a4032f
Jasper Phelps <jtmaniatesselvin@g.harvard.edu>
2022-01-28 20:07:24 -0500

    Add karmau alias for checking other users' karma
    Thanks to Wei for finding `sshare -a` which makes this possible

--- aliases_o2.sh
+++ aliases_o2.sh
@@ -10,6 +10,7 @@ alias interact="srun --pty -p interactive -t 0-12:00 --mem=2G bash"
 alias interactY="srun --pty -p interactive -t 0-12:00 --x11 --mem=2G bash"
 
 alias karma="sshare -U"
+alias karmau="sshare -a | grep"
 alias karmareport="sacct --format=jobid%9,jobName%30,state,MaxRSS,ReqMem%6,Elapsed%13,Timelimit,NCPUS%4,NodeList%20 --units=G -u jtm23"
 alias karmareportwide="sacct --format=jobid%9,jobName%50,state,MaxRSS,ReqMem%6,Elapsed%13,Timelimit,NCPUS%4,NodeList%20 --units=G -u jtm23"
 alias karmareportu="sacct --format=jobid%9,jobName%30,state,MaxRSS,ReqMem%6,Elapsed%13,Timelimit,NCPUS%4,NodeList%20 --units=G -u"
