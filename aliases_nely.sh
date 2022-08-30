2df17f6cbc9c44888fd4f889319efe7648c4451d
Jasper Phelps <jasper.s.phelps@gmail.com>
2022-08-30 17:35:30 +0200

    Add linux mount command for nely lab server

--- aliases_nely.sh
+++ aliases_nely.sh
@@ -1,7 +1,10 @@
 # Aliases for navigating filesystems and servers in the Neuroengineering Lab (PI: Pavan Ramdya)
 
-alias mntnely="mount -t smbfs //phelps@sv1files.epfl.ch/Ramdya-Lab $HOME/mnt/nelyserver"
-#alias mntnely="mount -t cifs //sv1files.epfl.ch/Ramdya-Lab ~/mnt/nelyserver -o username=phelps,noperm,vers=2.1,domain=intranet"  # This is what the lab manual says to use. Might work on linux but doesn't work for me on my mac.
+if $IS_LINUX; then
+	alias mntnely="sudo mount -t cifs //sv1files.epfl.ch/Ramdya-Lab $HOME/mnt/nelyserver -o username=phelps,noperm,vers=2.1,domain=intranet"
+elif $IS_MAC; then
+	alias mntnely="mount -t smbfs //phelps@sv1files.epfl.ch/Ramdya-Lab $HOME/mnt/nelyserver"
+fi
 alias cdnely="cd $HOME/mnt/nelyserver"
 alias mntnas="mount -t smbfs //phelps@upramdyanas1.epfl.ch/data ~/mnt/nelynas"
 alias cdnas="cd $HOME/mnt/nelynas"
