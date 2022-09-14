932d42248d66a51fd104fb06e737f56d5ba6ea8d
Jasper Phelps <jasper.s.phelps@gmail.com>
2022-09-14 12:20:22 +0200

    Update nely mount aliases

--- aliases_nely.sh
+++ aliases_nely.sh
@@ -1,15 +1,22 @@
 # Aliases for navigating filesystems and servers in the Neuroengineering Lab (PI: Pavan Ramdya)
 
 if $IS_LINUX; then
-	alias mntnely="sudo mount -t cifs //sv1files.epfl.ch/Ramdya-Lab $HOME/mnt/nelyserver -o username=phelps,noperm,vers=2.1,domain=intranet"
+	alias mntlabserv="sudo mount -t cifs //sv1files.epfl.ch/Ramdya-Lab /mnt/labserver -o username=phelps,noperm,vers=2.1,domain=intranet"
+    alias mntnas="sudo mount -t cifs //upramdyanas1.epfl.ch/data /mnt/nas -o username=phelps,noperm,vers=2.1,domain=intranet"
+    alias mntnas2="sudo mount -t cifs //upramdyanas1.epfl.ch/data2 /mnt/nas2 -o username=phelps,noperm,vers=2.1,domain=intranet"
+
+    alias cdlabserv="cd /mnt/labserver"
+    alias cdnas="cd /mnt/nas"
+    alias cdnas2="cd /mnt/nas2"
 elif $IS_MAC; then
-	alias mntnely="mount -t smbfs //phelps@sv1files.epfl.ch/Ramdya-Lab $HOME/mnt/nelyserver"
+	alias mntlabserv="mount -t smbfs //phelps@sv1files.epfl.ch/Ramdya-Lab $HOME/mnt/nely/labserver"
+    alias mntnas="mount -t smbfs //phelps@upramdyanas1.epfl.ch/data $HOME/mnt/nely/nas"
+    alias mntnas2="mount -t smbfs //phelps@upramdyanas1.epfl.ch/data2 $HOME/mnt/nely/nas2"
+
+    alias cdlabserv="cd $HOME/mnt/nely/labserver"
+    alias cdnas="cd $HOME/mnt/nely/nas"
+    alias cdnas2="cd $HOME/mnt/nely/nas2"
 fi
-alias cdnely="cd $HOME/mnt/nelyserver"
-alias mntnas="mount -t smbfs //phelps@upramdyanas1.epfl.ch/data ~/mnt/nelynas"
-alias cdnas="cd $HOME/mnt/nelynas"
-alias mntnas2="mount -t smbfs //phelps@upramdyanas1.epfl.ch/data2 ~/mnt/nelynas2"
-alias cdnas2="cd $HOME/mnt/nelynas2"
 
 j="$HOME/mnt/nelyserver/PHELPS_Jasper"
 alias cdj="if [ ! -e \"$j\" ]; then mntnely; fi; cd $j"
