69531c577a1a97625299fbf94f9d86cf1ce53a8c
Jasper Phelps <jasper.s.phelps@gmail.com>
2022-12-03 23:39:29 +0100

    Fix cdfanc

--- aliases/fanc.sh
+++ aliases/fanc.sh
@@ -1,5 +1,5 @@
 # Aliases for FANC, the Female Adult Nerve Cord EM dataset
 
 alias vifanc="vi $SHELL_CONFIGS_DIR/aliases/fanc.sh"
-alias cdfanc="cd $HOME/Dropbox*/FANC"
+alias cdfanc="cd $HOME/Dropbox*/Science/FANC"
 alias fancng="o 'https://neuromancer-seung-import.appspot.com/?json_url=https://global.daf-apis.com/nglstate/api/v1/6236020508983296'"
