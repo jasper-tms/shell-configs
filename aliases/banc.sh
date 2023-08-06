f71e6f47e22c2e02ed4967c830e5e6411d70da0b
Jasper Phelps <jasper.s.phelps@gmail.com>
2023-08-06 23:04:22 +0200

    Add `banc` aliases

---
+++ aliases/banc.sh
@@ -0,0 +1,5 @@
+# Aliases for the Brain And Nerve Cord (BANC) connectome project
+
+alias vibanc="vi $SHELL_CONFIGS_DIR/aliases/banc.sh"
+alias cdbanc="cd $HOME/Dropbox*/Science/BANC"
+alias ngbanc="o 'https://cj-find-path-test-dot-neuroglancer-dot-seung-lab.appspot.com/#!middleauth+https://global.daf-apis.com/nglstate/api/v1/6636514689679360'"
