5268e9439f468611424746216cff51c48ad3c4ff
Jasper Phelps <jasper.s.phelps@gmail.com>
2023-08-06 23:01:20 +0200

    Update FANC neuroglancer link

--- aliases/fanc.sh
+++ aliases/fanc.sh
@@ -2,4 +2,4 @@
 
 alias vifanc="vi $SHELL_CONFIGS_DIR/aliases/fanc.sh"
 alias cdfanc="cd $HOME/Dropbox*/Science/FANC"
-alias fancng="o 'https://neuromancer-seung-import.appspot.com/?json_url=https://global.daf-apis.com/nglstate/api/v1/6236020508983296'"
+alias fancng="o 'https://neuromancer-seung-import.appspot.com/?json_url=https://global.daf-apis.com/nglstate/api/v1/5472833633255424'"
