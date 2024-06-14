3a9432e74a17e2259c7471153101040c6c051361
Jasper Phelps <jasper.s.phelps@gmail.com>
2024-06-14 16:32:48 +0200

    Add aliases for mounting scape PC

--- aliases/nely.sh
+++ aliases/nely.sh
@@ -5,15 +5,19 @@ if $IS_LINUX; then
     alias mntls="mount /mnt/labserver"
     alias mntnas="mount /mnt/nas"
     alias mntnas2="mount /mnt/nas2"
+    alias mntscapepc="mount /mnt/scapepc"
 elif $IS_MAC; then
     # Use samba mounts because I couldn't get cifs to work on Mac
     alias mntls="mount -t smbfs //phelps@sv-nas1.rcp.epfl.ch/Ramdya-Lab $HOME/mnt/labserver"
     alias mntnas="mount -t smbfs //phelps@upramdyanas1.epfl.ch/data $HOME/mnt/nas"
     alias mntnas2="mount -t smbfs //phelps@upramdyanas1.epfl.ch/data2 $HOME/mnt/nas2"
+    alias mntscapepcphelps="mount -t smbfs //phelps@128.178.194.38/data $HOME/mnt/scapepc"
+    alias mntscapepc='echo Enter nely password; mount -t smbfs "//.;nely@128.178.194.38/data" $HOME/mnt/scapepc'
 fi
 alias cdls="if [ ! -e \"/mnt/labserver/data/JSP\" ]; then mntls; fi; cd /mnt/labserver/data/JSP"
 alias cdnas="if [ ! -e \"/mnt/nas/JSP\" ]; then mntnas; fi; cd /mnt/nas/JSP"
 alias cdnas2="if [ ! -e \"/mnt/nas2/JSP\" ]; then mntnas2; fi; cd /mnt/nas2/JSP"
+alias cdscapepc="if [ ! -e \"/mnt/scapepc/JSP_scape_data\" ]; then mntscapepc; fi; cd /mnt/scapepc/JSP_scape_data"
 
 
 alias cdpo="cd $HOME/Dropbox*/Science/'Postdoc - EPFL'"
