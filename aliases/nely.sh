fd3cbbfd2c0804134a08e6fef6b640f7afd403a8
Jasper Phelps <jasper.s.phelps@gmail.com>
2024-07-02 16:03:57 +0200

    Update nely labserver paths and mount points

--- aliases/nely.sh
+++ aliases/nely.sh
@@ -2,21 +2,20 @@
 
 if $IS_LINUX; then
     # Assume mount settings are listed in /etc/fstab
-    alias mntls="mount /mnt/labserver"
-    alias mntnas="mount /mnt/nas"
-    alias mntnas2="mount /mnt/nas2"
+    alias mntls="$HOME/repos/nely/knowledge-base/mount_servers.sh"
+    alias mntlsd="mount /mnt/upramdya_data"
+    alias mntlsf="mount /mnt/upramdya_files"
     alias mntscapepc="mount /mnt/scapepc"
 elif $IS_MAC; then
     # Use samba mounts because I couldn't get cifs to work on Mac
-    alias mntls="mount -t smbfs //phelps@sv-nas1.rcp.epfl.ch/Ramdya-Lab $HOME/mnt/labserver"
-    alias mntnas="mount -t smbfs //phelps@upramdyanas1.epfl.ch/data $HOME/mnt/nas"
-    alias mntnas2="mount -t smbfs //phelps@upramdyanas1.epfl.ch/data2 $HOME/mnt/nas2"
+    alias mntlsf="mount -t smbfs //phelps@sv-nas1.rcp.epfl.ch/upramdya/files $HOME/mnt/upramdya_files"
+    alias mntlsd="mount -t smbfs //phelps@sv-nas1.rcp.epfl.ch/upramdya/data $HOME/mnt/upramdya_data"
     alias mntscapepcphelps="mount -t smbfs //phelps@128.178.194.38/data $HOME/mnt/scapepc"
     alias mntscapepc='echo Enter nely password; mount -t smbfs "//.;nely@128.178.194.38/data" $HOME/mnt/scapepc'
 fi
-alias cdls="if [ ! -e \"/mnt/labserver/data/JSP\" ]; then mntls; fi; cd /mnt/labserver/data/JSP"
-alias cdnas="if [ ! -e \"/mnt/nas/JSP\" ]; then mntnas; fi; cd /mnt/nas/JSP"
-alias cdnas2="if [ ! -e \"/mnt/nas2/JSP\" ]; then mntnas2; fi; cd /mnt/nas2/JSP"
+alias cdlsd="if [ ! -e \"/mnt/upramdya_data/JSP\" ]; then mntlsd; fi; cd /mnt/upramdya_data/JSP"
+alias cdls=cdlsd
+alias cdlsf="if [ ! -e \"/mnt/upramdya_files/PHELPS_Jasper\" ]; then mntlsf; fi; cd /mnt/upramdya_files/PHELPS_Jasper"
 alias cdscapepc="if [ ! -e \"/mnt/scapepc/JSP_scape_data\" ]; then mntscapepc; fi; cd /mnt/scapepc/JSP_scape_data"
 
 
