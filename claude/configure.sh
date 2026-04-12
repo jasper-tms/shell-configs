cb93b0dbd3b87361e9f2001a67d625964ec55ce6
Jasper Phelps <jasper.s.phelps@gmail.com>
2026-04-12 11:03:01 +0200

    Prevent claude from changing settings.json

--- claude/configure.sh
+++ claude/configure.sh
@@ -6,3 +6,8 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd -P)"
 mkdir -p "$HOME/.claude" > /dev/null
 ln -sv "$SCRIPT_DIR/CLAUDE.md" "$HOME/.claude/CLAUDE.md"
 ln -sv "$SCRIPT_DIR/settings.json" "$HOME/.claude/settings.json"
+
+# Claude tries to update settings.json too much (e.g. user changes the model or
+# effort within a session -> claude tries to make that the new default for future
+# sessions by changing settings.json), so make it read-only to prevent that.
+chmod -w "$SCRIPT_DIR" "$SCRIPT_DIR/settings.json"
