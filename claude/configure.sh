627d70283e7cd5216f8f4f3763b3d0a5eab89c8a
Jasper Phelps <jasper.s.phelps@gmail.com>
2026-04-12 11:03:40 +0200

    Minor: Move a comment for clarity

--- claude/configure.sh
+++ claude/configure.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-# symlink files in this directory into $HOME/.claude where
-# Claude Code will actually see and use them
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd -P)"
+
+# symlink files in this directory into $HOME/.claude where
+# Claude Code will actually see and use them
 mkdir -p "$HOME/.claude" > /dev/null
 ln -sv "$SCRIPT_DIR/CLAUDE.md" "$HOME/.claude/CLAUDE.md"
 ln -sv "$SCRIPT_DIR/settings.json" "$HOME/.claude/settings.json"
